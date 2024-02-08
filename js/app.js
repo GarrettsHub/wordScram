9499b6a1ca1078ba5053d3d3d0477a5bbe171341
GarrettsHub <helixsparrk@gmail.com>
2024-02-08 11:51:29 -0600

    "making big money moves here"

--- js/app.js
+++ js/app.js
@@ -26,20 +26,38 @@ class WordScrambleGame {
             {
                 word: 'coding',
                 hint: 'what was done to make this game!'
+            },
+            {
+                word: 'forgot',
+                hint: 'wait what was i talking about again?'
+            },
+            {
+                word: 'spokesperson',
+                hint: 'something that your mom is when you go to the doctors office'
+            },
+            {
+                word: 'horseshoe',
+                hint: 'horses wear this with their calm luh fit'
+            },
+            {
+                word: 'absent',
+                hint: 'your father falls under this adjective'
             }
         ];
 
-        // 3. Initialize the game
+        // 3. Initialize the game (not sure if the positioning of this matters too much)
         this.init();
-        // 5. Add event listener for the "New Word" button
+        // 4. Add event listener for the my buttons (not sure why they only work in the constructor but it makes sense in a way)
         this.newWordButton.addEventListener('click', ()=> {
             this.shuffle();
+            this.clear();
         });
         this.checkButton.addEventListener('click', ()=> {
             this.check()
         })
     }
 
+    // initalizer 
     init() {
         this.shuffle()
         // this.correct()
@@ -60,10 +78,10 @@ class WordScrambleGame {
     }
 
 
-    // method to shuffle the characters of a word into a random order
+    // 5. method to shuffle the characters of a word into a random order
     shuffle() {
         let wordObj = this.words[Math.floor(Math.random() * this.words.length)]; // gets random object from words
-        let wordArr = wordObj.word.split(""); // Split the word into an array of characters
+        let wordArr = wordObj.word.split(""); // Split the word into an array of characters (thank you geek4geeks)
 
         this.correctWord = wordObj.word
 
@@ -73,7 +91,7 @@ class WordScrambleGame {
             [wordArr[i], wordArr[p]] = [wordArr[p], wordArr[i]];
         }
 
-        this.wordText.innerText = wordArr.join(''); // Display the scrambled word
+        this.wordText.innerText = wordArr.join(''); // Display the scrambled word (thanks stack overflow and geek4geeks)
         this.hintText.innerText = wordObj.hint; // Display the hint
     }
 
@@ -87,11 +105,11 @@ class WordScrambleGame {
 
     // }
 
-    // method for checking if the typed answer matches word array
+    // 6. method for checking if the typed answer matches word array
     check() {
         let answerBox = this.inputBox.value.toLowerCase()
         // let correctWord = this.correct()
-        let answer = this.wordText.innerText.toLowerCase().trim(); // Get the word displayed on the screen and convert it to lowercase
+        let answer = this.wordText.innerText.toLowerCase().trim(); // useless code left as a reminder of failure
 
         if (answerBox.trim() === '') {
             return  // if anything text is entered do nothing
@@ -101,12 +119,17 @@ class WordScrambleGame {
         // console.log(answerBox);
         if (answerBox !== this.correctWord) {
             this.message.innerText = `${answerBox} is obviously not the answer`
+            this.inputBox.value = ''
         } else {
             this.message.innerText = `I'm suprised you know that word`
         }
     }
 
-    
+    // 7. method to clear screen (for new word button) // use in new level later on
+    clear() {
+        this.inputBox.value = ''
+        this.message.innerText = ''
+    }
 }
 
 // Instantiate the WordScrambleGame class
---
+++ js/words&phrases.js
@@ -0,0 +1,18 @@
+words = [
+    {
+        word: 'family',
+        hint: 'Dom Torettos most prized possesion'
+    },
+    {
+        word: 'countryside',
+        hint: 'a place with no skyrise buildings. Just hard work and mean cows'
+    },
+    {
+        word: 'money',
+        hint: 'newtons first law of motion says you need some of this'
+    },
+    {
+        word: 'coding',
+        hint: 'what was done to make this game!'
+    }
+];
