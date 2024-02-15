baf9ab66930e29ffa707de4ac567310230808414
GarrettsHub <helixsparrk@gmail.com>
2024-02-14 20:44:22 -0600

    "we are so up"

--- js/app.js
+++ js/app.js
@@ -7,7 +7,14 @@ class WordScrambleGame {
         this.checkButton = document.getElementById('checkBtn')
         this.inputBox = document.getElementById('wordInput')
         this.message = document.getElementById('messageBox')
+        this.timer = document.querySelector('.timer')
+        this.overlay = document.getElementById('timeUp')
+        this.overlayText = document.querySelector('.time-up-content')
+        this.correctWord = ''
+        this.contentHold = document.querySelector('.content')
+        this.restart = document.querySelector('.restart-btn')
 
+        this.inputBox.focus()
 
         // 2. Define array of words and hints
         this.words = [
@@ -42,8 +49,17 @@ class WordScrambleGame {
             {
                 word: 'absent',
                 hint: 'your father falls under this adjective'
+            },
+            {
+                word: 'convict',
+                hint: 'do NOT call Al Capone this'
             }
         ];
+
+        // (forgot number but we still up) set score property?
+        this.score = {
+            currScore: 0
+        }
 
         // 3. Initialize the game (not sure if the positioning of this matters too much)
         this.init();
@@ -51,17 +67,29 @@ class WordScrambleGame {
         this.newWordButton.addEventListener('click', ()=> {
             this.shuffle();
             this.clear();
+            // this.clock()
+            // this.resetClock()
         });
         this.checkButton.addEventListener('click', ()=> {
             this.check()
+            
+        })
+        this.inputBox.addEventListener('keyup', function(event) {
+            if (event.key === 'Enter') {
+                event.preventDefault()
+                document.getElementById('checkBtn').click()
+            }
         })
     }
 
     // initalizer 
     init() {
         this.shuffle()
-        // this.correct()
         this.check()
+        // this.clock()
+        this.restartGame()
+        // this.shuffleTimer()
+        // this.correct()
         // 4. Select a random word from the array and its hint
         // let wordObj = this.words[Math.floor(Math.random() * this.words.length)];
         // let wordArr = wordObj.word.split(""); // Split the word into an array of characters
@@ -76,14 +104,23 @@ class WordScrambleGame {
         // this.wordText.innerText = wordArr.join(''); // Display the scrambled word
         // this.hintText.innerText = wordObj.hint; // Display the hint
     }
+    
+
+    // simple method for displaying socre!!!! (order is getting out of hand but we ball)
+    scoreKeep() {
+        const scoreDisplay = document.querySelector('.score')
+
+        scoreDisplay.innerText = this.score.currScore
+    }
 
 
     // 5. method to shuffle the characters of a word into a random order
     shuffle() {
         let wordObj = this.words[Math.floor(Math.random() * this.words.length)]; // gets random object from words
-        let wordArr = wordObj.word.split(""); // Split the word into an array of characters (thank you geek4geeks)
+        let wordArr = wordObj.word.split(""); // Split the word into an array of characters
 
         this.correctWord = wordObj.word
+        console.log(this.correctWord, wordObj.word);
 
         // Shuffle the characters of the word array (wordArr)
         for (let i = wordArr.length - 1; i > 0; i--) {
@@ -94,6 +131,12 @@ class WordScrambleGame {
         this.wordText.innerText = wordArr.join(''); // Display the scrambled word (thanks stack overflow and geek4geeks)
         this.hintText.innerText = wordObj.hint; // Display the hint
     }
+
+
+    //(no longer needed)
+    // shuffleTimer() {
+    //     setInterval(this.shuffle(), 3000)
+    // }
 
     // method for correct word that i apparently do not need
     // correct() {
@@ -109,7 +152,7 @@ class WordScrambleGame {
     check() {
         let answerBox = this.inputBox.value.toLowerCase()
         // let correctWord = this.correct()
-        let answer = this.wordText.innerText.toLowerCase().trim(); // useless code left as a reminder of failure
+        // let answer = this.wordText.innerText.toLowerCase().trim(); // useless code left as a reminder of failure
 
         if (answerBox.trim() === '') {
             return  // if anything text is entered do nothing
@@ -122,6 +165,11 @@ class WordScrambleGame {
             this.inputBox.value = ''
         } else {
             this.message.innerText = `I'm suprised you know that word`
+            this.shuffle()
+            this.inputBox.value = ''
+            this.inputBox.focus()
+            this.score.currScore+= 1
+            this.scoreKeep()
         }
     }
 
@@ -130,7 +178,53 @@ class WordScrambleGame {
         this.inputBox.value = ''
         this.message.innerText = ''
     }
+
+    clock() {
+        let timeLeft = 5
+        let timerId = setInterval(countdown, 1000);
+
+        function countdown() {  
+            console.log(timeLeft, game.timer);
+            if (timeLeft == 0) {
+                clearTimeout(timerId)
+                game.gameOver()
+            } else  {
+                timeLeft--
+                game.timer.innerText = timeLeft;
+            }
+        }
+    }
+
+    //(may no longer be needed)
+    // resetClock() {
+    //     // clearTimeout(this.timerId)
+    //     clearInterval(this.timerId)
+    //     // this.timer.innerText = ''
+    //     this.clock()
+
+        
+    // }
+
+    gameOver() {
+        this.overlay.classList.remove('d-none')
+        this.overlay.classList.add('d-block')
+        this.contentHold.classList.add('d-none')
+        // this.overlayText.innerText = 'Games over! Time to pick up a book or two'
+    }
+
+    restartGame() {
+        this.restart.addEventListener('click', ()=> {
+            this.overlay.classList.add('d-none')
+            this.overlay.classList.remove('d-block')
+            this.contentHold.classList.remove('d-none')
+            this.contentHold.classList.add('d-block')
+        })
+    }
+
+    
 }
+
+
 
 // Instantiate the WordScrambleGame class
 const game = new WordScrambleGame();
